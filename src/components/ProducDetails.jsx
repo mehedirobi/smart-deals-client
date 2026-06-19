24089f87f1f0f844f158efba4a367315437d992a
mehedirobi <mehedirobidev@gmail.com>
2026-06-19 20:03:13 +0600

    bid added

--- src/components/ProducDetails.jsx
+++ src/components/ProducDetails.jsx
@@ -1,7 +1,11 @@
-import React, { useRef } from "react";
+import React, { use, useRef } from "react";
 import { useLoaderData } from "react-router";
+import { Authcontext } from "../context/AuthContext";
 
 const ProducDetails = () => {
+
+  const {user} = use(Authcontext)
+
   const product = useLoaderData();
   const bidModalRef = useRef(null)
   console.log(product);
@@ -9,6 +13,12 @@ const ProducDetails = () => {
   const handleBidModal = () => {
     bidModalRef.current.showModal();
   }
+
+  const handleBitSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <div>
       <button
@@ -18,10 +28,28 @@ const ProducDetails = () => {
       
       <dialog ref={bidModalRef} id="my_modal_5" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
+          <h3 className="font-bold text-lg">Give the best offrer!</h3>
           <p className="py-4">
-            Press ESC key or click the button below to close
+            Offer something seller can not rassist
           </p>
+
+          {/* Form */}
+          <form onSubmit={handleBitSubmit}>
+             <fieldset className="fieldset">
+          <label className="label">Name</label>
+          <input type="email" className="input" defaultValue={user.displayName} readOnly />
+
+          <label className="label">Email</label>
+          <input type="email" readOnly className="input" defaultValue={user.email} />
+
+          <label className="label">Bid Amount</label>
+          <input type="number" className="input" placeholder="Enter Bid amount" />
+
+          <button className="btn btn-primary my-2">Place yout Bid</button>
+          
+          
+        </fieldset>
+          </form>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
